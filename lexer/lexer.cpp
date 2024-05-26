aecf6bd2bf436a44d1c117485aa9e58e5c472b8c
Bread <77288737+riley0122@users.noreply.github.com>
2024-05-26 23:57:16 +0200

    Added some data types and stuff ig

--- lexer/lexer.cpp
+++ lexer/lexer.cpp
@@ -1,5 +1,6 @@
 #include "lexer.hpp"
 
 namespace Orchid::Compiler::Frontend::Lexer {
-
+	Token::Token(TokenType t, const std::string& txt, int ln, int col, int idx)
+		: type(t), text(txt), line(ln), column(col), index(idx) {}
 }
--- lexer/lexer.hpp
+++ lexer/lexer.hpp
@@ -1,3 +1,42 @@
+#include <string>
 namespace Orchid::Compiler::Frontend::Lexer {
+	enum TokenType {
+		KEYWORD,
+		IDENTIFIER,
+		SEPERATOR,
+		LITERAL,
+		COMMENT,
+		WHITESPACE,
+		TYPE,
+		OPERATOR,
+	};
 
+	enum LiteralType {
+		NOTALITERAL,
+		STRINGLITERAL,
+		NUMERICLITERAL,
+		BOOLEANLITERAL,
+	};
+
+	enum TypeType {
+		NOTATYPE,
+		INT,
+		STRING,
+		BOOL,
+	};
+
+	struct Token {
+		TokenType type;
+
+		std::string text;
+		
+		int line;
+		int column;
+		int index;
+
+		LiteralType literal_type = LiteralType::NOTALITERAL;
+		TypeType type_type = TypeType::NOTATYPE;
+
+		Token(TokenType t, const std::string& txt, int ln, int col, int idx);
+	};
 }
