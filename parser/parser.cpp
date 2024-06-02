a60b7ceeffd049c93075dc77bdd203ef28880b15
Riley <77288737+riley0122@users.noreply.github.com>
2024-06-02 23:37:33 +0200

    Add loop with index

--- parser/parser.cpp
+++ parser/parser.cpp
@@ -1,15 +1,17 @@
 #include "parser.hpp"
 #include "../lexer/lexer.hpp"
 
+#include <iostream>
+
 namespace Orchid::Compiler::Frontend::Parser {
     Orchid::Compiler::Frontend::AST::Node generateAST(std::vector<Orchid::Compiler::Frontend::Lexer::Token> tokens) {
         Orchid::Compiler::Frontend::Lexer::Token current = tokens[0];
         Orchid::Compiler::Frontend::Lexer::Token lookahead = tokens[0];
-        int index = -1;
+        int* index = new int{-1};
 
         auto advance = [&]() {
-			current = tokens[index++];
-            lookahead = tokens[index+1];
+			current = tokens[++*index];
+            lookahead = tokens[*index+1];
 		};
 
         advance();
@@ -19,6 +21,14 @@ namespace Orchid::Compiler::Frontend::Parser {
             token: Orchid::Compiler::Frontend::Lexer::Token(Orchid::Compiler::Frontend::Lexer::TokenType::WHITESPACE, "[ROOT]", -1,-1,-1),
             subnodes: std::vector<Orchid::Compiler::Frontend::AST::Node>(), 
         };
+
+        while (*index < tokens.size()) {
+            if (current.type == Orchid::Compiler::Frontend::Lexer::TokenType::OPERATOR) {
+                std::cout << current.text;
+            }
+
+            advance();
+        }
 
         return root;
     }
