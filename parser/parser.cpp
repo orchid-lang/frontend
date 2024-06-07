16ff3524adb300100c5f168fdd2278afdec6a341
Riley <77288737+riley0122@users.noreply.github.com>
2024-06-07 18:52:07 +0200

    Add start and end keywords

--- parser/parser.cpp
+++ parser/parser.cpp
@@ -32,20 +32,34 @@ namespace Orchid::Compiler::Frontend::Parser {
             std::vector<Orchid::Compiler::Frontend::AST::Node>()
             );
 
+        Orchid::Compiler::Frontend::AST::Node currentParent = root;
+
         // Loop over all tokens but skipping the last
         while (*index < (signed)tokens.size()) {
             advance();
 
             // identifier name can't be keyword name
             if (current.type == Orchid::Compiler::Frontend::Lexer::TokenType::IDENTIFIER && vector_has(Orchid::Compiler::Frontend::Lexer::KEYWORDS, current.text)) {
-                throw std::runtime_error("Please report this! \nThis error shouldn't be possible! \nReport here: https://github.com/orchid-lang/frontend/issues \nError code: 302901");
+                throw std::runtime_error("Please report this! \nThis error shouldn't be possible! \nReport here: https://github.com/orchid-lang/frontend/issues");
             }
 
             switch (current.type)
             {
             case Lexer::KEYWORD:
-                // Handle keywords
-                break;
+                if (current.text == "start") {
+                    if (lookahead.type != Lexer::KEYWORD && lookahead.text != "start") {
+                        throw std::runtime_error(std::format("Loose 'start' keyword! (ln:{};cl:{},id:{})", current.line, current.column, current.index));
+                    } else {
+                        Orchid::Compiler::Frontend::AST::Node functionNode(Orchid::Compiler::Frontend::AST::NodeType::OPERATION, current, {});
+                        currentParent = functionNode;
+                    }
+                }
+
+                if (current.text == "end") {
+                    if (currentParent == root) {
+                        throw new std::runtime_error(std::format("Cannot end when not in function body! (ln:{};cl:{},id:{})", current.line, current.column, current.index));
+                    }
+                }
             case Lexer::IDENTIFIER:
                 // Handle identifiers
                 break;
